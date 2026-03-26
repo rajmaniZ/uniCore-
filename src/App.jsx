1181cb745694ff6eb524108ea36556f9e76acb07
rajmaniZ <mr.raj725sharma@gmail.com>
2026-03-27 01:20:43 +0530

    adding resister/login/forget password layout and route it

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Layout from './layout/layout';
 import Home from './pages/home/home';
@@ -7,8 +7,13 @@ import About from './pages/about/about';
 import Contact from './pages/contact/contact';
 
 import Login from './pages/login/login';
-import Register from './pages/login/register';
+import Register from './pages/login/register/collegeRegister';
 import ForgetPassword from './pages/login/forget-password';
+import RequestForAccount from './pages/login/register/requestForAccount'
+
+
+
+
 import ComingSoon from './pages/comingSoon/ComingSoon';
 // Dashboard
 import Admin from "./pages/Dashboard/AdminDashboard/Admin";
@@ -17,72 +22,80 @@ import Teacher from "./pages/Dashboard/TeacherDashboard/Teacher";
 import Principle from "./pages/Dashboard/PrincipleDashboard/Principle";
 
 
+import LoginLayout from './pages/login/loginLayout';
+
 // import Home from './pages/home/home';
 
-function App(){
-  const futurePages=[
-        {
-            path:"/career",
-            name:'Future of uniCore'
-        },
-        {
-            path:'/blog',
-            name:'Our Blogs'
-        },
-        {
-            path:'/docs',
-            name:'Documantation'
-        },
-        {
-            path:'/guide',
-            name:'How to use our features'
-        },
-        {
-            path:'/help',
-            name:'How we helped you!'
-        },
-        {
-            path:'/faqs',
-            name:'FAQs'
-        },
-        {
-            path:'/policy',
-            name:'privacy Guidelines'
-        },
-        {
-            path:'/terms',
-            name:"Terms of Service"
-        }
-    ];
+function App() {
+  const futurePages = [
+    {
+      path: "/career",
+      name: 'Future of uniCore'
+    },
+    {
+      path: '/blog',
+      name: 'Our Blogs'
+    },
+    {
+      path: '/docs',
+      name: 'Documantation'
+    },
+    {
+      path: '/guide',
+      name: 'How to use our features'
+    },
+    {
+      path: '/help',
+      name: 'How we helped you!'
+    },
+    {
+      path: '/faqs',
+      name: 'FAQs'
+    },
+    {
+      path: '/policy',
+      name: 'privacy Guidelines'
+    },
+    {
+      path: '/terms',
+      name: "Terms of Service"
+    }
+  ];
 
   return (
     <>
       <Routes>
-          <Route path='/' element={<Layout/>}>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/features' element={<Features/>}/>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/features' element={<Features />} />
 
           {/* upcoming pages */}
-          {futurePages.map((page, index)=>(
-          <Route 
-            key={index}
-            path={page.path}
-            element={<ComingSoon pageName={page.name}/>}
-          />
-        ))}
+          {futurePages.map((page, index) => (
+            <Route
+              key={index}
+              path={page.path}
+              element={<ComingSoon pageName={page.name} />}
+            />
+          ))}
 
 
         </Route>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/forget-password' element={<ForgetPassword/>}/>
-        <Route path='/Admin' element={<Admin/>}/>
-          <Route path='/Principle' element={<Principle/>}/>
-            <Route path='/Teacher' element={<Teacher/>}/>
-              <Route path='/Student' element={<Student/>}/>
-        
+
+
+        <Route path="/" element={<LoginLayout/>}>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forget-password' element={<ForgetPassword />} />
+          <Route path='/RequestForAccount' element={<RequestForAccount/>}/>
+          
+        </Route>
+        <Route path='/Admin' element={<Admin />} />
+        <Route path='/Principle' element={<Principle />} />
+        <Route path='/Teacher' element={<Teacher />} />
+        <Route path='/Student' element={<Student />} />
+
       </Routes>
     </>
   )
