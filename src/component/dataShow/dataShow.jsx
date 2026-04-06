5ab51158d9df728f4091ff10abb62fc56703473b
ankshra28 <2805ayumishra@gmail.com>
2026-04-06 03:34:57 -0700

    addingDashboardHeader,sidebar,dataShow

---
+++ src/component/dataShow/dataShow.jsx
@@ -0,0 +1,13 @@
+
+import styles from './dataShow.module.css';
+function ShowData(){
+  return (
+    <>
+    <div className={styles.dataPage
+    }>
+     
+    </div>
+    </>
+  );
+}
+export default ShowData;
