354b9d41e358abd2e86bdde13b7ca9e5f355e8a6
ankshra28 <2805ayumishra@gmail.com>
2026-04-07 04:01:33 -0700

    makin dashboard layout

--- src/pages/Dashboard/PrincipleDashboard/Principle.jsx
+++ src/pages/Dashboard/PrincipleDashboard/Principle.jsx
@@ -2,14 +2,14 @@ import DashboardHeader from "../../../component/dashboardHeader/header";
 import DashboardSidebar from './../../../component/DashboardSidebar/Sidebar'
 // import ShowData from './../../../component/dataShow/dataShow'
 import styles from './Principle.module.css';
-import ShowList from "../../../component/showList/showList";
+// import ShowList from "../../../component/showList/showList";
 function Principle(){
     return(
         <>
-        <DashboardHeader/>
+        {/* <DashboardHeader/> */}
         <div className={styles.ShowData}>
-        <DashboardSidebar/>
-        <ShowList/>
+        {/* <DashboardSidebar/> */}
+        {/* <ShowList/> */}
         {/* <ShowData/> */}
         </div>
         
