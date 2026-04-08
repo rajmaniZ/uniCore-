ff53d28628fb74852608a5faa3e006d782607fb1
ankshra28 <2805ayumishra@gmail.com>
2026-04-08 02:56:03 -0700

    fixing header and sidebar

--- src/component/DashboardSidebar/Sidebar.jsx
+++ src/component/DashboardSidebar/Sidebar.jsx
@@ -4,50 +4,7 @@ function Sidebar() {
     <>
       <div className={styles.SidebarDiv}>
         <ul>
-          <li>Dashboard</li>
-          <li>
-            Departments
-            <select>
-              <option className={styles.option}>
-                {" "}
-                IT
-                <select>
-                  <option>Tacher</option>
-                </select>
-              </option>
-              <option option className={styles.option}>
-                CS
-              </option>
-              <option className={styles.option}> ECE</option>
-              <option option className={styles.option}>
-                EE
-              </option>
-              <option className={styles.option}> ME</option>
-              <option option className={styles.option}>
-                CE
-              </option>
-              <select>
-                <li>Teacher</li>
-                <option option className={styles.option} value="">
-                  Year
-                </option>
-                <select>
-                  <option option className={styles.option} value="1">
-                    First Year
-                  </option>
-                  <option option className={styles.option} value="2">
-                    Second Year
-                  </option>
-                  <option option className={styles.option} value="3">
-                    Third Year
-                  </option>
-                  <option option className={styles.li} value="4">
-                    Fourth Year
-                  </option>
-                </select>
-              </select>
-            </select>
-          </li>
+          
           <li>Teacher</li>
           <li>Students</li>
           <li>Dashboard</li>
