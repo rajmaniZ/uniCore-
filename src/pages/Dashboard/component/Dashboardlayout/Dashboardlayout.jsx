f7b146d5ba08b116bcb6ed638f3c5ef29b150824
rajmaniZ <mr.raj725sharma@gmail.com>
2026-04-30 17:26:06 +0530

    fixed  some aichat img import

--- src/pages/Dashboard/component/Dashboardlayout/Dashboardlayout.jsx
+++ src/pages/Dashboard/component/Dashboardlayout/Dashboardlayout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom';
 import Sidebar from './../dSidebar/dSidebar';
 import Navbar from './../navbar/navbar';
 import styles from './Dashboardlayout.module.css';
-import chatAI from "chatWithAI.png"
+import chatAI from "/chatWithAi.png"
 
 
 function DashboardLayout() {
